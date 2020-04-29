d729705079793f1aa2582dd2d6295dcf73bb8872
isaac-3 <isaacgc0596@gmail.com>
2020-04-29 13:49:50 -0500

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (arr) => {
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(" ")
+    let capWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    return capWords.join(" ")
+  })
 }
